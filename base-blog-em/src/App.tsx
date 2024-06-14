1e06a3a2e36f2b0c916ce5ebdbf27c0dbc56cee5
MaorM <maormenashe2@gmailcom>
2024-06-14 20:15:37 +0300

    Adding Query Client and a Provider

--- base-blog-em/src/App.tsx
+++ base-blog-em/src/App.tsx
@@ -1,12 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import Posts from "./components/posts/Posts";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Blog &apos;em Ipsum</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog &apos;em Ipsum</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
