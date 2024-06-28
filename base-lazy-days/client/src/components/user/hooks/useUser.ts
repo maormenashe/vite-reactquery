adfaea711ac78f4cde074790dcbc84d8ed7515ac
MaorM <maormenashe2@gmailcom>
2024-06-28 11:15:11 +0300

    enables, Infinity staleTime, setQueryData, removeQueries

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,35 +1,46 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
 import type { User } from "@shared/types";
 
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
+import { generateUserKey } from "@/react-query/key-factories";
 import { queryKeys } from "@/react-query/constants";
 
-// query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const queryClient = useQueryClient();
+  const { userId, userToken } = useLoginData();
 
-  // meant to be called from useAuth
+  const { data: user } = useQuery({
+    enabled: !!userId,
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: () => getUser(userId, userToken),
+    staleTime: Infinity,
+  });
+
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    queryClient.setQueryData(
+      generateUserKey(newUser.id, newUser.token),
+      newUser
+    );
   }
 
-  // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    queryClient.removeQueries({
+      queryKey: [queryKeys.user],
+    });
   }
 
   return { user, updateUser, clearUser };
